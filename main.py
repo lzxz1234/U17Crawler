27b10e2917c9e2cb5ff969fa1cacbc0efc966f0e
lzxz1234 <314946925@qq.com>
2015-02-08 23:27:28 +0800

    重置按钮添加逻辑

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import sys
 
 from ui.MainWindow import Ui_MainWindow
 from PyQt4 import QtGui
-
+from PyQt4 import Qt
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
+        self.button_reset.connect(self.button_reset,
+                                  Qt.SIGNAL("clicked()"),
+                                  lambda : self.input_book_name.setText(''))
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
